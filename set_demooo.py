7ecf8c2da61916705b499e3a994a63cf95181d88
Reza Tirgir <parsatirgir20@gmail.com>
2026-07-08 13:20:11 +0330

    second commit for 'set' data type

--- set_demooo.py
+++ set_demooo.py
@@ -8,7 +8,6 @@ sampleset = {1,'A',3.86,'b',4,9,'reza','A','A','A'}
 for _ in range(10):
   print(sampleset)
 
-print('=' *40)
 
 odd = set(range(1 ,50,2))   #members: 25
 power = {1,4,9,16,25,36,49}  #members: 7
@@ -17,3 +16,10 @@ power = {1,4,9,16,25,36,49}  #members: 7
 print(len(odd))
 print(odd.intersection(power))
 print(len(odd.union(power)))
+
+print('=' *40)
+print(odd.intersection_update(power))
+print(odd)
+
+power_frozen = frozenset(power)
+power_frozen.
